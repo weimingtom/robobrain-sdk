735f62746060fae1921af9e6764122ba56afbf71
robobrainsdk@gmail.com <robobrainsdk@gmail.com@fb033ca5-b669-49a8-0d26-d434464765f1>
2011-12-08 02:41:16 +0000

    Beta 2.2

---
+++ tags/beta 2.2/src/org/robobrain/sdk/GameActivity.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2011 James Johnson
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ */
+
+package org.robobrain.sdk;
+
+import org.robobrain.sdk.audio.MusicManager;
+import org.robobrain.sdk.audio.SoundManager;
+import org.robobrain.sdk.game.Engine;
+import org.robobrain.sdk.graphics.Color;
+import org.robobrain.sdk.graphics.TextureManager;
+import org.robobrain.sdk.input.Multitouch;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * A GameActivity creates a full screen OpenGL window and provides methods for 
+ * registering and your game's Engine object with the Renderer. This class is not 
+ * meant to be instantiated. Derive your game's Activity from this class.
+ * @author James Johnson
+ */
+public class GameActivity extends Activity {
+	/**
+	 * The game's Engine. Will be set to your Game class.
+	 */
+	protected Engine mGame;
+	
+	/**
+	 * The GameActivity's Context.
+	 */
+	protected static Context sContext;
+	
+	/**
+	 * A View that manages the OpenGL Renderer.
+	 */
+	protected GLView mGLView;
+	
+	/**
+	 * Called when the activity is starting. This is where most initialization 
+	 * should go.
+	 */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Create a fullscreen window
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        					 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        					 
+        
+        // Init sound system
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        
+        sContext = this;
+        
+        Multitouch.clear();
+    }
+    
+    /**
+     * Called after onRestoreInstanceState(Bundle), onRestart(), or onPause(), for your 
+     * activity to start interacting with the user.
+     */
+    @Override 
+    protected void onResume() {
+    	super.onResume();
+    	if (mGame != null) {
+    		mGame.play();
+    	}
+    	if (mGLView != null) {
+    		mGLView.onResume();
+    	}
+    	
+	}
+    
+    /**
+     * Called as part of the activity lifecycle when an activity is going into the 
+     * background, but has not (yet) been killed. 
+     */
+    @Override 
+    protected void onPause() {
+    	super.onPause();
+    	TextureManager.unloadAll();
+    	if (mGame != null) {
+    		mGame.pause();
+    	}
+    	if (mGLView != null) {
+    		mGLView.onPause();
+    	}
+    	SoundManager.stopAll();
+    	MusicManager.pause();
+    }
+    
+    /**
+     * Perform any final cleanup before an activity is destroyed. This can happen either because 
+     * the activity is finishing (someone called finish() on it, or because the system is 
+     * temporarily destroying this instance of the activity to save space. You can distinguish 
+     * between these two scenarios with the isFinishing() method. 
+     */
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	TextureManager.release();
+    	SoundManager.release();
+    	MusicManager.release();
+    	if (mGame != null) {
+    		mGame.shutdown();
+    	}
+    	System.gc();
+    	mGame = null;
+    	sContext = null;
+    	mGLView  = null;
+    }
+    
+    /**
+     * Sets the background color of the Activity
+     */
+    public void setBackgroundColor(Color color) {
+    	mGLView.setBackgroundColor(color);
+    }
+    
+    /**
+     * Registers a game Engine class to be used by this Activity to run a game.
+     */
+    public void registerGame(Engine game) {
+    	if (mGLView == null) {
+    		Log.e("GameActivity", "Null GLView passed into registerGame.");
+    		Log.e("GameActivity", "Did you forget to call initRenderer() in onCreate()?");
+    		return;
+    	}
+    	mGLView.registerGame(game);
+    }
+    
+    /**
+     * Initializes the OpenGL renderer. Call this in your derived Activity's onCreate() method.
+     * @param width
+     * Width in pixels of the desired screen.
+     * @param height
+     * Height in pixels of the desired screen.
+     */
+    public void initRenderer(int width, int height) {
+    	// Initialize the Renderer
+        mGLView = new GLView(this);
+        setContentView(mGLView);
+        setTargetSize(width, height);
+    }
+    
+    /**
+     * Sets the ideal width and height for your game. This will be used to 
+     * calculate the ratio to scale your scene assets by in order to fit the 
+     * player's phone screen.
+     * @param Width in pixels of the screen.
+     * @param Height in pixels of the screen.
+     */
+    public void setTargetSize(int width, int height) {
+    	mGLView.setTargetSize(width, height);
+    }
+    
+    /**
+     * Gets the Activity's application Context.
+     * @return A valid application Context.
+     */
+    public static Context getContext() { return sContext; }
+}
---
+++ tags/beta 2.2/src/org/robobrain/test/SoundActivity.java
@@ -0,0 +1,52 @@
+package org.robobrain.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import org.robobrain.test.R;
+import org.robobrain.sdk.GameActivity;
+import org.robobrain.sdk.audio.SoundManager;
+
+// Your Activity must be derived from GameActivity for the Sound system to work.
+public class SoundActivity extends GameActivity {
+	final static int SND_ONE_UP = 21;
+	final static int SND_COIN = 22;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sound);
+        
+        SoundManager.registerSound("sounds/one_up.wav", SND_ONE_UP);
+        SoundManager.registerSound("sounds/coin.wav", SND_COIN);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		SoundManager.stopAll();
+		//SoundManager.unloadAll();
+		//SoundManager.release();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		//SoundManager.unloadAll();
+		SoundManager.release();
+	}
+
+	
+	public void onPlayBtnClick(View view) {
+		SoundManager.play(SND_ONE_UP);
+	}
+	
+	public void onLoopBtnClick(View view) {
+		SoundManager.play(SND_COIN, true);
+	}
+	
+	public void onStopBtnClick(View view) {
+		SoundManager.stop(SND_COIN);
+	}
+}
